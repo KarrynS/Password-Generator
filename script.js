d4cb60c9bd478b9ad91b1066f29fe10cb2386013
Phoenix <73591532+KarrynS@users.noreply.github.com>
2020-11-28 02:02:47 +1100

    first script and html drafts

--- script.js
+++ script.js
@@ -1,77 +1,88 @@
-var button = document.querySelector("#generateButton");
-
 // Establishing possible characters
+
 var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialChar = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "{", "|", "}", "~", + '"'];
 
-//Establishing variables for password
-var passwordLength = parseInt(prompt("Choose a password length between 8 - 128 characters",""));
-
-  if (passwordLength < 8 || passwordLength > 128){
-    prompt("Your number (" + passwordLength + ") is outside the range of [8-128]. Choose again!");
-  }
-  if (passwordLegth >= 8 || passwordLength <= 128) {
-  }
-  else{
-  var userLowerCase = confirm ("Would you like to use lower case letters in your password?");
-  var userUpperCase = confirm ("Would you like to use uppercase letters in your password?");
-  var userNumbers = confirm ("Would you like to use numbers in your password?");
-  var userSpecialChar = confirm ("Would you like to use special characters in your password?");
-  }
-
-//Generating appropriate password length
-
-
-  // Establishing variables
-var password = "";
-var allParameters = {}; // Object includes all possible parameters
-var userParameters = {}; // Object includes only parameters selected by user
-var parameterCount = 0
-var possibleChars = "";
-
-
-//Establishing Possible useparameters
-// if user selects to use only one of the password criteria
- lowerCase.charAt()
-
-//if user selects to use 2 of the 4 password criteria
-
-
-
-//if user selects to use 3 of the 4 criteria
-
-
-//if user selects to use 4 of the 4 criteria
-
-
-
-// Assignment Code
+// Connecting DOM elements to HTML
 var generateBtn = document.querySelector("#generate");
+var resultEl = document.querySelector("#passwordbox");
+var lengthBox = document.querySelector("#length");
+var lowerCaseSwitch = document.querySelector("#lowercase");
+var upperCaseSwitch = document.querySelector("#uppercase");
+var numbersSwitch = document.querySelector("#numbers");
+var specialCharSwitch = document.querySelector("#special");
+
+// User input of parameters 
+  var length = parseInt(lengthBox.value);
+  var hasLowerCase = lowerCaseSwitch.checked;
+  var hasUpperCase = upperCaseSwitch.checked;
+  var hasNumbers = numbersSwitch.checked;
+  var hasSpecialChar = specialCharSwitch.checked;
+
+//generate password with parameters
+generatePassword(hasLowerCase, hasUpperCase, hasNumbers, hasSpecialChar, length);
+
+//Generate password function 
+function generatePassword (lower, upper, number, specialChar, length) {
+
+  let generatedPassword = " ";
+  var typesCount = lower + upper + number + specialChar;
+  var typesArr = [{lower }, {upper}, {number}, {specialChar}].filter(item => Object.values(item)[0]);
+
+  if(typesCount === 0) {
+    return '';
+  }
+
+  for(let i=0; i < length; i+= typesCount) {
+    typesArr.forEach(type => {
+      var funcName = Object.keys(type)[0];
+
+      generatedPassword += randomFunc[funcName]();
+    });
+  }
+  var password = generatedPassword;
+  
+  return generatedPassword;
+}
+//Establishing randomized parameters
+const randomFunc = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  special: getRandomSpecial,
+};
+  //Randomising lowercase
+  function getRandomLower () {
+      return lowerCase [Math.floor(Math.random() * lowerCase.length)];
+  }
+  //Randomising uppercase
+  function getRandomUpper () {
+    return upperCase [Math.floor(Math.random() * upperCase.length)];
+  }
+  //Randomising numbers
+  function getRandomNumber () {
+    return numbers [Math.floor(Math.random() * numbers.length)];
+  }
+  //Randominsion special characters
+  function getRandomSpecial () {
+    return specialChar [Math.floor(Math.random() * specialChar.length)];
+  }
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
+  var password = generatedPassword();
+  var passwordText = document.querySelector("#passwordbox");
   passwordText.value = password;
 
 }
 
+//Vaildating Password criteria
+if (length < 8 || length > 128) {
+  alert("Please enter a password length between 8-128");
+} else if (typecount = 0)
+  alert("Atleast one password criteria must be selected")
+
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+generateBtn.addEventListener("click", writePassword)
